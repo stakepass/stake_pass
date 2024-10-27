fb262dcfab0d54be243aa8324da313c557ec9c87
stakepass <stakepass123456@proton.me>
2024-10-27 14:43:51 +0100

    new_files

--- www/file.js
+++
@@ -1,7 +0,0 @@
-		document.addEventListener('DOMContentLoaded', function() {
-  			window.startZxing();
-			parent.c3_callFunction("OnMarkerClose", ["param1", "param2"]);
-		});
-
-function startZxing(){
-};
--- www/scripts/c3main.js
+++
@@ -1,4 +0,0 @@
-import "./c3runtime.js";
-import "./objRefTable.js";
-import "./project/main.js";
-import "./project/scriptsInEvents.js";
